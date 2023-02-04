bfcd870012577b386be538cd835c92deed733555
Matheus <matheusfelipegomesfernandes@gmail.com>
2023-02-04 12:27:16 -0300

    Adicionando Function countDown

--- JS/index.js
+++ JS/index.js
@@ -7,9 +7,11 @@ const minutesDisplay = document.querySelector('.minutes')
 const secondsDisplay = document.querySelector('.seconds')
 const initialMinutes = minutesDisplay.textContent
 const initialSecond = secondsDisplay.textContent
+let timeTimerOut
 let newMinutes;
 
 buttonPlay.addEventListener('click', function() {
+    countDown()
     buttonPlay.classList.add('hide')
     buttonPause.classList.remove('hide')
 })
@@ -46,5 +48,33 @@ function resetTimer () {
 
 buttonStop.addEventListener('click', function() {
     resetTimer()
+    clearTimeout(timeTimerOut)
 })
 
+function countDown() {
+    timeTimerOut = setTimeout(function(){
+        let seconds = Number(secondsDisplay.textContent)
+        let minutes = Number(minutesDisplay.textContent)
+        if (seconds <= 0) {
+            seconds = 59
+            minutes--
+        } else {
+            seconds--
+        }
+
+        minutesDisplay.textContent = String(minutes)
+        secondsDisplay.textContent = String(seconds)
+        countDown()
+
+    }, 1000)
+}
+
+const rainSounds = new Audio("https://github.com/AlexJjunio/my-projects/raw/main/pomodoro-2.0/sounds/rain.wav?raw=true")
+const forestSounds = new Audio("https://github.com/AlexJjunio/my-projects/raw/main/pomodoro-2.0/sounds/florest.wav?raw=true")
+const coffeShopSounds = new Audio("https://github.com/AlexJjunio/my-projects/raw/main/pomodoro-2.0/sounds/coffeshop.wav?raw=true")
+const fireplaceSounds = new Audio("https://github.com/AlexJjunio/my-projects/raw/main/pomodoro-2.0/sounds/fireplace.wav?raw=true")
+
+const rainSoundsButton = document.querySelector('.rainSoundsButton')
+const forestSoundsButton = document.querySelector('.forestSoundsButton')
+const coffeShopSoundsButton = document.querySelector('.coffeShopSoundsButton')
+const fireplaceSoundsButton = document.querySelector('.fireplaceSoundsButton')
